ba308c266a7d382b7f58a88e4aeb0bca0c055a02
Ario <purogamingx@gmail.com>
2025-11-15 12:02:16 +0100

    PASTED

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+// Hide console window on Windows
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+mod types;
+mod steam;
+mod non_steam;
+mod backup;
+mod sync;
+mod gui;
+mod config;
+
+use eframe::egui;
+use gui::SaveGuardianApp;
+
+fn main() -> Result<(), eframe::Error> {
+    // Initialize logging
+    env_logger::init();
+    
+    // Set up eframe options
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([1200.0, 800.0])
+            .with_min_inner_size([800.0, 600.0]),
+        ..Default::default()
+    };
+    
+    // Run the application
+    eframe::run_native(
+        "Save Guardian",
+        options,
+        Box::new(|cc| Box::new(SaveGuardianApp::new(cc))),
+    )
+}
